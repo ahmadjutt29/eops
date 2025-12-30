6b770a80f580a7e07b89a0fd704c5c28bd3a8245
ahmad <mr.ahmadshakeel@gmail.com>
2025-12-30 15:39:24 +0500

    chore: major changes in vite config and workflow

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  
   return {
     plugins: [react()],
     // Set base path based on environment
